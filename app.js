6ab367dd728669795395a6bc801e465c46135848
jimenaianina <jime.ianina@gmail.com>
2021-01-10 09:40:56 -0300

    añadiendo estilo

---
+++ app.js
@@ -0,0 +1,11 @@
+app.listen(3030,()=> console.log("inicio del servidor"));
+
+const path=require("path");
+
+app.use(express.static(path.resolve(__dirname,"public")));
+
+app.get("/",(req,res)=> res.sendFile(path.resolve(__dirname,"views","home.html")));
+
+app.get("/login",(req,res)=> res.sendFile(path.resolve(__dirname,"views","login.html")));
+
+app.get("/register",(req,res)=> res.sendFile(path.resolve(__dirname,"views","register.html")));
