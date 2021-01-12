dcac211e2f1708a7da79d000dd073b46bc5f7824
jimenaianina <jime.ianina@gmail.com>
2021-01-12 19:34:31 -0300

    .

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.listen(3000,()=> console.log("inicio del servidor"));
+app.listen(process.env.PORT || 3000,()=> console.log("inicio del servidor"));
 
 const path=require("path");
 
