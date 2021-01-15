a3b098b923f8dfaa557b9ad4563ef2930a60c940
jimenaianina <jime.ianina@gmail.com>
2021-01-14 23:07:23 -0300

    .

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.use(express.static(path.resolve(__dirname,"public")));
 
 app.post("/",(req,res)=> res.sendFile(path.resolve(__dirname,"views","home.html")));
 
+app.post("/login",(req,res)=> res.sendFile(path.resolve(__dirname,"views","login.html")));
+
+app.post("/register",(req,res)=> res.sendFile(path.resolve(__dirname,"views","register.html")));
+
 app.get("/",(req,res)=> res.sendFile(path.resolve(__dirname,"views","home.html")));
 
 app.get("/login",(req,res)=> res.sendFile(path.resolve(__dirname,"views","login.html")));
